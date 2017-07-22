16aaef142ba7a8d9d9a1d79f8b177835e2d35a72
Carlos Hernandez <rhernandez@kenzan.com>
2017-07-22 11:34:40 -0400

    updated http call to use jsonp module

--- src/services/gravatar.service.ts
+++ src/services/gravatar.service.ts
@@ -1,6 +1,5 @@
 import {Md5} from 'ts-md5/dist/md5';
-// import jsonp = require('jsonp');
-import axios from 'axios';
+import jsonp = require('jsonp');
 
 interface GravatarInterface {
   displayName?: string;
@@ -17,26 +16,18 @@ interface EntryInterface {
 }
 
 export function getAvatar(email: string): Promise<string> {
-  const gravatarUrl: string = '/gravatar.com/';
+  const gravatarUrl: string = '//www.gravatar.com/';
   const emailHash: string = Md5.hashStr(email).toString();
   const url: string = `${gravatarUrl}${emailHash}.json`;
 
-  const config = {
-    headers: {
-      'Access-Control-Allow-Methods': 'GET,PUT,PATCH,POST,DELETE',
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json'
-    },
-  }
-
   return new Promise((resolve, reject) => {
-    axios
-      .get(url, config)
-      .then(res => {
-        resolve(res.data.entry[0].thumbnailUrl);
-      })
-      .catch((e) => {
-        reject(e);
-      });
+    jsonp(url, null, (err: {}, data: EntryInterface) => {
+      if (err) {
+        reject(err);
+        console.log('in reject');
+      } else {
+        resolve(data.entry[0].thumbnailUrl);
+      }
+    });
   });
 }

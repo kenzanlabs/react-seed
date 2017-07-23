a7620c49ab675bf233cea924b0333dd24246d742
Owen Buckley <obuckley@kenzan.com>
2017-07-23 14:08:24 -0400

    variable rename

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -11,9 +11,9 @@ export interface ContactInterface {
 export default class ContactService {
 
   public static buildName(contact: ContactInterface): string {
-    const _middleName: string = contact.middleName ? ` ${contact.middleName}` : '';
-    const _lastName: string = contact.lastName ? ` ${contact.lastName}` : '';
+    const middleName: string = contact.middleName ? ` ${contact.middleName}` : '';
+    const lastName: string = contact.lastName ? ` ${contact.lastName}` : '';
 
-    return `${contact.firstName}${_middleName}${_lastName}`;
+    return `${contact.firstName}${middleName}${lastName}`;
   }
 }

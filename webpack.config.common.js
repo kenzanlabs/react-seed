67f8c1c32b46ae8fd3180c4b220e82d469caf610
Christopher Joslyn <christopher.joslyn@gmail.com>
2017-08-02 16:51:50 -0400

    add support for hot module replacement (#53)
    * add support for hot module replacement
    
    - fix output name for HMR
    - fix HMR for sass files
    - install react hot loader
    
    Resolves #25
    
    * config tweaks
    
    * fixed broken webpack schema

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,5 +1,4 @@
 const chalk = require('chalk');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
@@ -33,6 +32,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loaders: [
+          'react-hot-loader',
           'awesome-typescript-loader'
         ],
         exclude: path.join(__dirname, 'node_modules')
@@ -47,13 +47,6 @@ module.exports = {
         test: /\.html$/,
         loader: 'html-loader',
         exclude: path.join(__dirname, './index.html')
-      },
-      {
-        test: /\.(s*)css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -79,11 +72,6 @@ module.exports = {
     new ProgressBarPlugin({
       format: chalk.blue('  build ') + '[:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
       clear: false
-    }),
-
-    new ExtractTextPlugin({
-      filename: 'styles.css',
-      allChunks: true
     }),
 
     new webpack.ProvidePlugin({ // exposes non-modular vendor globals to webpack
